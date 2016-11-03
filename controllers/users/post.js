76c5c24fb5e9cab397bf0333e5e6f90b5954c56f
Julian Coleman <julcol03@gmail.com>
2016-11-03 16:43:11 -0700

    rename get to post

--- controllers/users/post.js
+++ controllers/users/post.js
@@ -1,10 +1,12 @@
 const Service = appRequire('lib/users/post/service');
 const Validator = appRequire('lib/users/post/validator');
+
 const ResponseHelpers = appRequire('helpers/responses');
 const UserAlreadyExistsError = appRequire('lib/errors/user_already_exists_error');
 
 const post = (request, reply) => {
   const payload = request.payload;
+
   Service.createUser(payload)
     .then(response => reply(response.toJSON()).code(201))
     .catch(UserAlreadyExistsError, err => reply.boom(err.code, err.message))
