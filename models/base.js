964dfa5c072b0cc15407d6d83fdd32308282c5a9
Julian Coleman <julcol03@gmail.com>
2016-11-04 12:59:47 -0700

    simplify parse and format; remove unnecessary monkeypatching

--- models/base.js
+++ models/base.js
@@ -1,68 +1,15 @@
 const Bookshelf = require('../config/bookshelf');
 const R = require('ramda');
 const string = require('underscore.string');
-const Hashids = require('hashids');
-
-const hashids = new Hashids(process.env.HASHIDS_SECRET_KEY);
-const encodeKey = key => hashids.encode(key) || key;
-const decodeKey = key => hashids.decode(key)[0] || null;
-
-const getIdKeys = obj => R.filter(R.test(/id/i), R.keys(obj));
-const hasKeys = keys => R.gt(R.length(keys), 0);
-const transformHashedKeys = (object, hashKeys, hashFunction) =>
-  R.zipObj(hashKeys, R.map(hashFunction, R.props(hashKeys, object)));
 
 const mapKeys = R.curry((fn, obj) =>
   R.fromPairs(R.map(R.adjust(fn, 0), R.toPairs(obj))));
 
-const parseExistingModel = (context, { attributes }) =>
-  new context.constructor(attributes, { parse: true });
-
 module.exports = Bookshelf.Model.extend({
   hasTimestamps: true,
   // convert snake_case to camelCase
-  parse: attrs => {
-    const camelizedObject = mapKeys(string.camelize, attrs);
-    const idKeys = getIdKeys(camelizedObject);
-
-    return camelizedObject;
-  },
+  parse: attrs => mapKeys(string.camelize, attrs),
 
   // convert camelCase to snake_case
-  format: attrs => {
-    const underscoredObject = mapKeys(string.underscored, attrs);
-    const idKeys = getIdKeys(underscoredObject);
-
-    return underscoredObject;
-  },
-
-  save(...args) {
-    this.id = decodeKey(this.id);
-    return Bookshelf.Model.prototype.save.apply(this, args)
-      .then(model => {
-        if (!model) {
-          return parseExistingModel(this, model);
-        }
-
-        const savedModel = model;
-        savedModel.attributes = { id: model.attributes.id };
-
-        return model.fetch()
-          .then(refreshedModel => parseExistingModel(this, refreshedModel))
-          .catch(err => {
-            console.error('Post-save fetch error');
-            throw err;
-          });
-      });
-    },
-
-  destroy(...args) {
-    this.id = decodeKey(this.id);
-    return Bookshelf.Model.prototype.destroy.apply(this, args);
-  },
-
-  forge(attributes, options) {
-    const args = [attributes, R.merge(options || {}, { parse: true })];
-    return Bookshelf.Model.prototype.forge.apply(this, args);
-  },
+  format: attrs => mapKeys(string.underscored, attrs),
 });
