62d191ee0f87652e41823b7b262e14d322d4aea1
Julian Coleman <julcol03@gmail.com>
2016-11-07 09:42:07 -0800

    remove console.log(); simplify arrow functions; remove unnecessary newlines

--- controllers/users/get.js
+++ controllers/users/get.js
@@ -1,28 +1,25 @@
 const Service = appRequire('lib/users/get/service');
-
 const ResponseHelpers = appRequire('helpers/responses');
 const UserNotFoundError = appRequire('lib/errors/user_not_found_error');
 
-const getUsersPromise = (user, query) => {
-  if (query) {
-    return Service.searchUsers(user, query);
-  }
+const getUsersPromise = (user, query) =>
+  query ? Service.searchUsers(user, query) : Service.getUsers(user);
 
-  return Service.getUsers(user);
-};
+  // if (query) {
+  //   return Service.searchUsers(user, query);
+  // }
+  //
+  // return Service.getUsers(user);
 
 const getUsers = (request, reply) => {
-  console.log('getting all users');
-
   getUsersPromise(request.user, request.query.q)
     .then(response => reply(response.toJSON()).code(200))
     .catch(err => ResponseHelpers.logErrorAndRespond(err, reply));
 };
 
 const getUser = (request, reply) => {
-  console.log('getting a user');
-
   const userId = request.params.id;
+
   Service.getUser(request.user, userId)
     .then(response => reply(response.toJSON()).code(200))
     .catch(UserNotFoundError, err => reply.boom(err.code, err.message))
